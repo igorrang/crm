0a0969df58bd65e52ff058ecfd4b00fe068428ab
Igor rangel <igormenezesrangel@gmail.com>
2024-10-18 13:45:30 -0300

    alterações

--- frontend/src/app/VerCliente/page.tsx
+++ frontend/src/app/VerCliente/page.tsx
@@ -209,41 +209,12 @@ export default function VerCliente() {
           <div className="flex flex-col items-center lg:flex-row lg:items-start lg:justify-start py-10 lg:px-20 ">
             {/* Onde mostrará os anexos de cada cliente */}
             <div className="w-[90%] max-w-[500px] lg:max-w-[500px] h-[522px] py-5 px-5 bg-secondary relative border border-white/70 shadow-md rounded-2xl lg:rounded-r-none overflow-auto " >
-              {dataDeposito.map((item) => (
-                <Button variant="clean" size="clean" className="w-full" onClick={() => {exibirDepositosCliente(item.id_deposito)}}>
-                  <CardFiltroAnexo data={item.data} hora={item.hora} valorFichas={item.valorFichas} valorReais={item.valorReais} anexo=""/>
-                </Button>
-              ))}
+              
             </div>
             
             {/* Dados detalhados do anexo */}
             <div className="bg-secondary flex flex-col justify-between w-[90%] max-w-[500px] h-[522px] lg:mr-5  mt-5 lg:mt-0 lg:mr-5 p-5 rounded-2xl lg:rounded-l-none border border-white/70 shadow-md">
-              {dataDepositoEscolhido.map((item) => (
-                <div>
-                {/* Imagem do anexo */}
-                <div className="relative w-full h-[120px] bg-cover bg-center hover:brightness-90" style={{ backgroundImage: "url('/Planilha.png')" }}>
-                  <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-400 bg-transparent">
-                    <p className="text-white text-lg">Clique para abrir</p>
-                  </div>
-                </div>
-                <div className="flex justify-between py-2 border-b-2 text-white mt-5 ">
-                  <h1 className="text-sm">Data:</h1>
-                  <h1 className="text-sm">{item.data}</h1>
-                </div>
-                <div className="flex justify-between py-2 border-b-2 text-white">
-                  <h1 className="text-sm">Hora:</h1>
-                  <h1 className="text-sm">{item.hora}</h1>
-                </div>
-                <div className="flex justify-between py-2 border-b-2 text-white">
-                  <h1 className="text-sm">Valor Reais:</h1>
-                  <h1 className="text-sm">{item.valorReais}</h1>
-                </div>
-                <div className="flex justify-between py-2 border-b-2 text-white">
-                  <h1 className="text-sm">Valor Fichas:</h1>
-                  <h1 className="text-sm">{item.valorFichas}</h1>
-                </div>
-              </div>
-              ))}
+              
               
               <div className="w-full flex justify-end">
                 <DialogEditarFichas identificador_props="" data_props="" hora_props="" valorReais_props="" valorFicha_props="" anexo_props=""></DialogEditarFichas>
