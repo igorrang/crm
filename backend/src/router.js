138edc246f839f89dee15465ef2fcf350ad98d79
Igor rangel <igormenezesrangel@gmail.com>
2024-10-22 11:16:15 -0300

    Update Router

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,7 +2,11 @@ const express = require("express")
 
 const router = express.Router()
 
+const routerValidacaoLogin = require("./router/validacaoLoginRota.js")
+const routerTablesCrud = require('/Users/igorrangelkonvictus/crm/backend/src/Routers/tableControllers.js')
 
+const routerLogin = express.Router(routerValidacaoLogin)
+const routerTables = express.Router(routerTablesCrud)
 
 // console.log(this.delete)
-module.exports = router
+module.exports = routerLogin
