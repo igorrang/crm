d6785c42d935527930ffcb430eacf40fc60611a4
Marcos Miguel Biedermann da Silva <131807683+marcos06k@users.noreply.github.com>
2024-10-23 13:34:27 -0300

    Versao mais atualizada

--- frontend/src/app/api/filtroTable/route.ts
+++ frontend/src/app/api/filtroTable/route.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const res = await axios.post('http://localhost:5050/filtroTable', body);
+    const res = await axios.post('http://frontend-backend-ff5606:5050/filtroTable', body);
     const data = res.data;
     return NextResponse.json(data);
   } catch (error) {
--- frontend/src/app/api/table/route.ts
+++ frontend/src/app/api/table/route.ts
@@ -16,7 +16,7 @@ export async function GET() {
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const res = await axios.post('http://localhost:5050/tables', body);
+    const res = await axios.post('http://frontend-backend-ff5606:5050/tables', body);
     const data = res.data;
     return NextResponse.json(data);
   } catch (error) {
@@ -28,7 +28,7 @@ export async function POST(request: Request) {
 export async function PUT(request: Request) {
   try {
     const body = await request.json();
-    const res = await axios.put('http://localhost:5050/tables', body);
+    const res = await axios.put('http://frontend-backend-ff5606:5050/tables', body);
     const data = res.data;
     return NextResponse.json(data);
   } catch (error) {
