c6d2d84ebfab19233b02c2d5debe22d0ea6f94cf
Marcos Miguel Biedermann da Silva <131807683+marcos06k@users.noreply.github.com>
2024-10-23 14:41:31 -0300

    adicionado arquivo layout em todas as pastas de telas

---
+++ frontend/src/app/VerCliente/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["300", "600"] });
+
+export const metadata: Metadata = {
+  title: "Crm Konvictus",
+  description: "Desenvolvido por Konvictus",
+  icons: {
+    icon: "KonvictusLogo.png"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
