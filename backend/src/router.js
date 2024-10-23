dd8cbe5173fd681677c0b34a35764238a8c5e9a4
Marcos Miguel Biedermann da Silva <131807683+marcos06k@users.noreply.github.com>
2024-10-23 13:42:37 -0300

    vir a partir daqui o backend do igor

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,7 +3,7 @@ const express = require("express")
 const router = express.Router()
 
 const routerValidacaoLogin = require("./router/validacaoLoginRota.js")
-const routerTablesCrud = require('/Users/igorrangelkonvictus/crm/backend/src/Routers/tableControllers.js')
+const routerTablesCrud = require('./router/tableControllers.js')
 
 const routerLogin = express.Router(routerValidacaoLogin)
 const routerTables = express.Router(routerTablesCrud)
