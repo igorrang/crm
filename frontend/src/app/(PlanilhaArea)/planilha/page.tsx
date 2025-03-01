e9521a9feeb1c0aeae57c2cf5bdcc51a31bf7df5
Igor rangel <igormenezesrangel@gmail.com>
2025-02-28 22:38:51 -0300

    ajuste

---
+++ frontend/src/app/(PlanilhaArea)/planilha/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import DialogCadastrarCliente from "@/components/DialogCadastrarCliente";
+import Header from "@/components/Header";
+import Navbar from "@/components/Narbar";
+import { DataTable } from "@/components/DataTable";
+import { ExcelUpload } from "@/components/ExcelUpload";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+
+
+export default function Planilha() {
+  return (
+    <main>
+      <Header></Header>
+      <div className="flex">
+        <Navbar></Navbar>
+
+        <div className="w-full min-h-[94vh]  text-black bg-gradient-to-l from-black via-black/90 to-black/85 ">
+          <div className="hidden lg:flex w-full py-4 px-10">
+           
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink asChild>
+                    
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbLink asChild>
+                   
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          
+          <div className="container mx-auto py-10">
+            <DialogCadastrarCliente></DialogCadastrarCliente>
+            <ExcelUpload />
+            <DataTable></DataTable>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
