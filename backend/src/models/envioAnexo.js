ac939bd716de08ffb62e6b687a8ad39f0f4337f9
Igor Rangel <115603531+igorrang@users.noreply.github.com>
2025-03-02 17:12:23 -0300

    Delete backend directory

--- backend/src/models/envioAnexo.js
+++
@@ -1,9 +0,0 @@
-const connection = require('./connection');
-
-const getClienteById = async (id_cliente) => {
-    const sql = `SELECT * FROM deposito WHERE id_cliente='${id_cliente}'`;
-    const [rows] = await connection.execute(sql);
-    return rows;
-};
-
-module.exports = { getClienteById };
--- backend/src/router/routerAnexo.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { armazenarConteudo } = require('../controllers/envioAnexoControllers.js');
-
-const anexo = express.Router();
-anexo.use(express.json());
-anexo.post('/armazenar', armazenarConteudo);
-
-module.exports = anexo;
