08862e35d2e5b502bf43e686a4b65baa35cd2a1e
Igor rangel <igormenezesrangel@gmail.com>
2025-03-10 14:17:41 -0300

    ajustes

--- frontend/src/components/Narbar/index.tsx
+++ frontend/src/components/Narbar/index.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
     <div className="min-w-[80px] hidden xl:flex flex-col  items-center  bg-gradient-to-t from-black/90 via-black/85 to-black/80 shadow-xl">
       <div className="fixed top-1/2 transform -translate-y-1/2">
         <div className="my-10 flex flex-col items-center justify-center text-white">
-          <Link href="/Principal" className="flex flex-col items-center">
+          <Link href="/principal" className="flex flex-col items-center">
             <MdOutlineDashboard className="text-[25px]"/>
             <h1 className="text-[12px] text-center">principal</h1>
           </Link>
