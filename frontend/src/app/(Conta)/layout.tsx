552ffbad51150c289164cc09a55eebfe17e9571c
Igor rangel <igormenezesrangel@gmail.com>
2025-03-10 13:35:40 -0300

    ajustes das rotas para deploy

---
+++ frontend/src/app/(Conta)/layout.tsx
@@ -0,0 +1,35 @@
+import '../globals.css'
+import type {Metadata} from 'next'
+import {DM_Sans} from 'next/font/google'
+import NextAuthSessionProvider from '@/models/providers/sessionProvider'
+import {getServerSession} from 'next-auth'
+import { nextAuthOption } from '@/service/utils/AuthOptions'
+
+const DMSans = DM_Sans({subsets: ['latin']})
+
+export const metadata: Metadata = {
+    title: 'Conta- crm',
+    description: ' -Crm Konvictus'
+}
+
+export default async function PlanilhaAreaLayout({
+    
+    children
+}: {
+    children: React.ReactNode
+}) {
+    const session = await getServerSession(nextAuthOption)
+    if (session) {
+   
+    }
+
+    return (
+        <html lang="pt-br">
+            <body
+            className={`${DMSans.className} flex flex-col h-screen w-screen`}
+            >
+                <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
+            </body>
+        </html>
+    )
+}
