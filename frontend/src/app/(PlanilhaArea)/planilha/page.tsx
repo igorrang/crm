a8567a4892b1a8785de24c1067090bb92b537b08
Igor Rangel <115603531+igorrang@users.noreply.github.com>
2025-03-27 00:17:55 -0300

    Update page.tsx
    correção de excelupdate

--- frontend/src/app/(PlanilhaArea)/planilha/page.tsx
+++ frontend/src/app/(PlanilhaArea)/planilha/page.tsx
@@ -2,30 +2,26 @@
 
 import DialogCadastrarCliente from "@/components/DialogCadastrarCliente";
 import Header from "@/components/Header";
-import Navbar from "@/components/Narbar";
+import Navbar from "@/components/Navbar"; // Corrigido aqui
 import { DataTable } from "@/components/DataTable";
 import { ExcelUpload } from "@/components/ExcelUpload";
-
-
-
 
 export default function Planilha() {
   return (
     <main>
-      <Header></Header>
+      <Header />
       <div className="flex">
-        <Navbar></Navbar>
+        <Navbar />
 
         <div className="w-full min-h-[94vh] text-black bg-gradient-to-l from-black via-black/90 to-black/85">
           <div className="hidden lg:flex w-full py-4 px-10">
-           
-           
+            {/* Pode adicionar Breadcrumbs ou outras informações aqui */}
           </div>
           
           <div className="container mx-auto py-10">
-            <DialogCadastrarCliente></DialogCadastrarCliente>
+            <DialogCadastrarCliente />
             <ExcelUpload />
-            <DataTable></DataTable>
+            <DataTable />
           </div>
         </div>
       </div>
