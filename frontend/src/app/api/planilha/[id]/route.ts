d7a48f164b88753f938596f0d16b3532cd35b2dc
Igor Rangel <115603531+igorrang@users.noreply.github.com>
2025-03-27 12:09:06 -0300

    Update route.ts

--- frontend/src/app/api/planilha/[id]/route.ts
+++ frontend/src/app/api/planilha/[id]/route.ts
@@ -1,33 +1,51 @@
 import { NextResponse } from 'next/server';
 import PlanilhaTabela from '@/models/Planilha';
 import { connectMongoDB } from '@/service/lib/mongodb';
+import * as XLSX from 'xlsx';
 
-export async function DELETE(
-    request: Request,
-    { params }: { params: { id: string } }
-) {
+export async function POST(request: Request) {
     try {
-        await  connectMongoDB();
-        const id = params.id;
+        await connectMongoDB();
 
-        const deletedPlanilha = await PlanilhaTabela.findByIdAndDelete(id);
+        const formData = await request.formData();
+        const file = formData.get('file');
 
-        if (!deletedPlanilha) {
+        if (!file || !file.name.endsWith('.xlsx')) {
             return NextResponse.json(
-                { error: 'Registro não encontrado' },
-                { status: 404 }
+                { error: 'Por favor, envie um arquivo Excel válido.' },
+                { status: 400 }
             );
         }
 
+        const reader = new FileReader();
+        reader.onload = async (event) => {
+            const binaryStr = event.target?.result;
+            const workbook = XLSX.read(binaryStr, { type: 'binary' });
+            const firstSheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[firstSheetName];
+            const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+            // Salvar os dados no MongoDB
+            const savedRecords = await PlanilhaTabela.insertMany(jsonData);
+            return NextResponse.json(
+                { message: 'Dados importados com sucesso!', records: savedRecords },
+                { status: 200 }
+            );
+        };
+
+        reader.readAsBinaryString(file);
+
+        // Retornar uma resposta padrão enquanto o FileReader processa o arquivo
         return NextResponse.json(
-            { message: 'Registro excluído com sucesso' },
-            { status: 200 }
+            { message: 'Processando arquivo...' },
+            { status: 202 }
         );
+
     } catch (error) {
-        console.error('Erro ao excluir:', error);
+        console.error('Erro ao importar planilha:', error);
         return NextResponse.json(
-            { error: 'Erro ao excluir registro' },
+            { error: 'Erro ao importar planilha' },
             { status: 500 }
         );
     }
-} 
+}
